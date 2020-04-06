1fee6a8fa261b88e93b8855e78fa22b05083eb4a
NotNV6 <patricktiborz@gmail.com>
2020-04-06 22:38:50 +0200

    add rank list command :sparkles:

--- Ranks/src/main/java/rip/skyland/ranks/commands/rank/RankCommand.java
+++ Ranks/src/main/java/rip/skyland/ranks/commands/rank/RankCommand.java
@@ -2,11 +2,14 @@ package rip.skyland.ranks.commands.rank;
 
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import rip.skyland.commons.CommonsPlugin;
 import rip.skyland.commons.command.annotation.Command;
 import rip.skyland.commons.command.annotation.Subcommand;
+import rip.skyland.commons.util.CC;
 import rip.skyland.ranks.commands.rank.procedure.RankCreateProcedure;
 import rip.skyland.ranks.commands.rank.procedure.RankDeleteProcedure;
 import rip.skyland.ranks.commands.rank.procedure.edit.RankEditChooseNameProcedure;
+import rip.skyland.ranks.ranks.RankModule;
 
 public class RankCommand {
 
@@ -36,5 +39,19 @@ public class RankCommand {
     @Subcommand(parentLabel = "rank", label = "edit", permission = "admin")
     public void executeEdit(final Player player) {
         new RankEditChooseNameProcedure(player).start();
+    }
+
+    @Subcommand(parentLabel = "rank", label = "list", permission = "admin")
+    public void executeList(final Player player) {
+        final RankModule rankModule = CommonsPlugin.getInstance().getHandler().findModule(RankModule.class);
+
+        player.sendMessage("");
+
+        rankModule.getRanks().stream()
+                .map(rank -> ChatColor.BLUE + "* " + CC.translate(rank.getDisplayName()) + ChatColor.AQUA + " (W: " + rank.getWeight() + ") ")
+                .forEach(player::sendMessage);
+
+        player.sendMessage("");
+
     }
 }
